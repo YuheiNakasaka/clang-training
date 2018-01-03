e0f224acaa926b7908d576574b27957b30b8a799
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2018-01-03 21:44:23 +0900

    バブルソートを改造

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,30 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 10
+
+void rand_array(int *array) {
+  srand((unsigned int)time(NULL));
+   for (int i = 0; i < N; i++) {
+     array[i] = rand();
+   }
+}
+
+void output_array(int *array) {
+  for (int i = 0; i < N; i++) {
+    printf("%d ", array[i]);
+  }
+  printf("\n");
+}
 
 // バブルソート
 int main() {
-  int array[] = {2,5,4,7,8,3,1,7,9,0,6};
-  int length = sizeof(array) / sizeof(array[0]);
+  int array[N];
+  rand_array(array);
+  output_array(array);
 
-  int sorted = length;
-  while(sorted > 1) {
-    sorted = length;
-    for (int i = 0; i < length; i++) {
-      // 最後の要素の一つ前の要素まで
-      if (i + 1 < length) {
-        if (array[i] > array[i + 1]) {
-          int tmp = array[i];
-          array[i] = array[i + 1];
-          array[i + 1] = tmp;
-        } else {
-          // ソートしてない回数をかぞえる、0になったらすべてソートされたことを意味する
-          sorted--;
-        }
+  int flag;
+  int k = 0;
+  do {
+    flag = 0;
+    for (int i = 0; i < N - 1 - k; i++) {
+      if (array[i] > array[i + 1]) {
+        int tmp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = tmp;
+        flag = 1;
       }
     }
-  }
+    k++;
+  } while(flag == 1);
 
-  for(int i = 0; i < length; i++) {
-    printf("%d ", array[i]);
-  }
+  output_array(array);
   return 0;
 }
