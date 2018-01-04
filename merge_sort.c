035d0ae41fc48f5e92f3477aa18438e4d7c5596c
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2018-01-04 10:09:49 +0900

    解説

--- merge_sort.c
+++ merge_sort.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define N 6
+#define N 8
 
 void rand_array(int *array) {
   srand((unsigned int)time(NULL));
@@ -56,9 +56,11 @@ void mergeSort(int *data, int start, int end) {
 }
 
 // マージソート
+// 分割統治で配列を２分した末端の左端の[1,2]から処理していって、[3,4]->[2,1],[4,3]->[4,3,2,1]と処理をする
+// 次に右側の配列にも同様の処理をして[8,7,6,5]にする
+// 最後に[4,3,2,1]と[8,7,6,5]を[8,7,6,5,4,3,2,1]にマージしておわり
 int main() {
-  int array[N];
-  rand_array(array);
+  int array[N] = {1,2,3,4,5,6,7,8};
   output_array(array);
 
   mergeSort(array, 0, N - 1);
